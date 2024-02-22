916f8af4f2163d555f49055525bbd8610164ea3b
rikif <rikif@LAPTOP-P9LPHMTP>
2024-02-22 09:05:25 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace es_lab_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserire la dimensione");
+            int dim = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[dim];
+            int[] pesi = new int[dim];
+            int[] età = new int[dim];
+        }
+    }
+}
