af3cec0e91658851c2c83d76ca37d7767c9a8e9c
rikif <rikif@LAPTOP-P9LPHMTP>
2024-02-22 09:17:52 +0100

    prima funzione

--- Program.cs
+++ Program.cs
@@ -8,8 +8,19 @@ namespace es_lab_12
             Console.WriteLine("inserire la dimensione");
             int dim = Convert.ToInt32(Console.ReadLine());
             int[] array = new int[dim];
-            int[] pesi = new int[dim];
+            double[] pesi = new double[dim];
             int[] età = new int[dim];
+            CaricaVettori(ref pesi, ref età);
+        }
+
+        static void CaricaVettori(ref double[] pesi,ref int[] età)
+        {
+            Random random = new Random();
+            for(int i = 0; i < età.Length; i++)
+            {
+                pesi[i] = random.Next(50,101);
+                età[i] = random.Next(18, 100);
+            }
         }
     }
 }
