61c63069c6fa0639cd515b16a8c0df34713b3866
rikif <rikif@LAPTOP-P9LPHMTP>
2024-02-23 09:03:18 +0100

    fit:completato la parte di main

--- Program.cs
+++ Program.cs
@@ -5,13 +5,30 @@ namespace es_lab_12
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("inserire la dimensione");
-            int dim = Convert.ToInt32(Console.ReadLine());
-            int[] array = new int[dim];
+            Console.WriteLine("Inserire la dimensione dell'array")  int dim =
+
+            Convert.ToInt32(Console.ReadLine());
             double[] pesi = new double[dim];
-            int[] età = new int[dim];
-            CaricaVettori(ref pesi, ref età);
+            int[] eta = new int[dim];
+            CaricaVettori(ref pesi ref età);
+            double media = 0;
+            double min = 0;
+            double max = 0;
+
+
+
+            Statistiche(ref pesi, ref media, ref min, ref max);
+            Console.WriteLine($"La media dei pesi è {media}, il valore minimo è {min} e il valore massimo {max}");
+
+            Ordina(ref pesi, ref eta);
+            for (int i = 0; i < dim; i++)
+            {
+                Console.WriteLine($"peso {i 1}: {pesi[i]}");
+                Console.WriteLine($"età {i + 1}: {eta[i]}\n");
+            }
+
         }
+    }                                                         
 
         static void CaricaVettori(ref double[] pesi,ref int[] età)
         {
@@ -41,12 +58,12 @@ namespace es_lab_12
             }
             media=somma/pesi.Length;
         }
-        static void Ordina(ref double[]pesi,ref int[] età)
+        static void Ordina(ref double[]pesi,ref int[]età)
         {
             double tempPesi;
             int tempEtà;
 
-            for (int i = 0;età.Length; i++)
+            for (int i = 0;età.Length ; i++)
             {
                 for(int j = 0;j< età.Length - 1 - i; j++)
                 {
